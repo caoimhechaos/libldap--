a5d1319343bd6e629e283486c4fae8e8383fb97c
Tonnerre Lombard <tonnerre@ancient-solutions.com>
2010-11-09 00:43:28 +0100

    Add the SimpleBind and SetDebuglevel functions we defined.

--- connection.cc
+++ connection.cc
@@ -1,3 +1,6 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
 #include <string>
 #include <ldap++.h>
 #include <ldap.h>
@@ -6,9 +9,21 @@ namespace ldap
 {
 LDAPConnection::LDAPConnection(std::string uri)
 {
-	int rc;
+	int rc = ldap_initialize(&this->_ldap, uri.c_str());
+	if (rc)
+		LDAPErrCode2Exception(rc);
+}
 
-	rc = ldap_initialize(&this->_ldap, uri.c_str());
+void LDAPConnection::SetDebuglevel(int newlevel)
+{
+	int rc = ldap_set_option(this->_ldap, LDAP_OPT_DEBUG_LEVEL, &newlevel);
+	if (rc)
+		LDAPErrCode2Exception(rc);
+}
+
+void LDAPConnection::SimpleBind(std::string user, std::string password)
+{
+	int rc = ldap_simple_bind_s(this->_ldap, user.c_str(), password.c_str());
 	if (rc)
 		LDAPErrCode2Exception(rc);
 }
