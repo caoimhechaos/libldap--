6c3d921d2339717fa527199339d2babefa4d8aca
Caoimhe Chaos <caoimhechaos@protonmail.com>
2010-11-14 15:05:01 +0100

    Add an entry class associated with the result object.

--- result.cc
+++ result.cc
@@ -3,6 +3,7 @@
 #endif
 #include <string>
 #include <vector>
+#include <ext/hash_map>
 #include <ldap++.h>
 #include <ldap.h>
 
@@ -11,10 +12,22 @@ namespace ldap_client
 LDAPResult::LDAPResult(LDAPConnection* conn, LDAPMessage* msg)
 : _conn(conn), _msg(msg)
 {
+	LDAPMessage *e = ldap_first_entry(_conn->_ldap, _msg);
+
+	do
+	{
+		_entries.push_back(LDAPEntry(_conn, e));
+	}
+	while ((e = ldap_next_entry(_conn->_ldap, e)) != NULL);
 }
 
 LDAPResult::~LDAPResult()
 {
 	ldap_msgfree(_msg);
+}
+
+std::vector<LDAPEntry>* LDAPResult::GetEntries()
+{
+	return &_entries;
 }
 }
