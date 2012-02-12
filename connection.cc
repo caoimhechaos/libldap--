80a500bcd680ce305fa849c3e5f6eba8f6a3c03e
Tonnerre Lombard <tonnerre@ancient-solutions.com>
2012-02-12 15:16:43 +0100

    Implement methods to create and modify LDAP entries. Also, add some documentation to the methods, they really deserve it.

--- connection.cc
+++ connection.cc
@@ -3,11 +3,16 @@
 #endif
 #include <string>
 #include <vector>
-#include <ldap++.h>
+#include "ldap++.h"
 #include <ldap.h>
 
 namespace ldap_client
 {
+/**
+ * Sets the system-wide LDAP debug level.
+ *
+ * @param newlevel New debug level (see the LDAP manual).
+ */
 void LDAPSetDebuglevel(int newlevel)
 {
 	int rc = ldap_set_option(NULL, LDAP_OPT_DEBUG_LEVEL, &newlevel);
@@ -19,6 +24,12 @@ void LDAPSetDebuglevel(int newlevel)
 		LDAPErrCode2Exception(rc);
 }
 
+/**
+ * Set the LDAP CA certificate file.
+ *
+ * @param path Path to the CA certificate file the LDAP server certificate
+ *             has been signed with.
+ */
 void LDAPSetCACert(std::string path)
 {
 	int rc;
@@ -28,7 +39,12 @@ void LDAPSetCACert(std::string path)
 		LDAPErrCode2Exception(rc);
 }
 
-
+/**
+ * Establish a new LDAP connection to the given URI.
+ *
+ * @param uri     URI of the LDAP server.
+ * @param version LDAP version to use (LDAP_VERSION2 or LDAP_VERSION3).
+ */
 LDAPConnection::LDAPConnection(std::string uri, int version)
 {
 	int rc = ldap_initialize(&_ldap, uri.c_str());
@@ -38,11 +54,19 @@ LDAPConnection::LDAPConnection(std::string uri, int version)
 	SetVersion(version);
 }
 
+/**
+ * Disconnect from the LDAP server, also unbind.
+ */
 LDAPConnection::~LDAPConnection()
 {
-	ldap_unbind(_ldap);
+	ldap_unbind_ext_s(_ldap, 0, 0);
 }
 
+/**
+ * Retrieve the most recently occurred error message.
+ *
+ * @return String containing the most recent error message.
+ */
 std::string LDAPConnection::GetLastError()
 {
 	char *str;
@@ -55,6 +79,12 @@ std::string LDAPConnection::GetLastError()
 	return std::string(str);
 }
 
+/**
+ * Set the LDAP version used in the connection.
+ *
+ * @param newversion New LDAP version (LDAP_VERSION2 or LDAP_VERSION3).
+ * @throws LDAPError Invalid LDAP version or unable to set.
+ */
 void LDAPConnection::SetVersion(int newversion)
 {
 	int rc;
@@ -67,13 +97,37 @@ void LDAPConnection::SetVersion(int newversion)
 		LDAPErrCode2Exception(rc);
 }
 
+/**
+ * Perform LDAP authentication using a simple BIND.
+ * You should use SASLBind instead.
+ *
+ * @param user     LDAP user name (typically a DN).
+ * @param password LDAP password as a string.
+ * @throws LDAPError Unable to perform bind.
+ */
 void LDAPConnection::SimpleBind(std::string user, std::string password)
 {
-	int rc = ldap_simple_bind_s(this->_ldap, user.c_str(), password.c_str());
+	struct berval passwd;
+	int rc;
+
+	passwd.bv_val = strdup(user.c_str());
+	passwd.bv_len = user.length();
+
+	rc = ldap_sasl_bind_s(_ldap, user.c_str(), LDAP_SASL_SIMPLE, &passwd,
+			0, 0, 0);
+
+	free(passwd.bv_val);
 	if (rc)
 		LDAPErrCode2Exception(rc);
 }
 
+/**
+ * Perform SASL bind. Currently, essentially the same as simple bind.
+ *
+ * @param user     LDAP user name (typically a DN).
+ * @param password LDAP password as a string.
+ * @throws LDAPError Unable to perform bind.
+ */
 void LDAPConnection::SASLBind(std::string user, std::string password)
 {
 	LDAPControl **sctrlsp = NULL, **cctrlsp = NULL;
@@ -91,6 +145,18 @@ void LDAPConnection::SASLBind(std::string user, std::string password)
 		LDAPErrCode2Exception(rc);
 }
 
+/**
+ * Search for LDAP records matching a given filter.
+ *
+ * @param base    Search base to start looking from.
+ * @param scope   LDAP search scope (e.g. ONE, SUB, etc.)
+ * @param filter  Filter string (e.g. attribute=value).
+ * @param attrs   Vector of attributes to fetch in the record.
+ * @param timeout Number of milliseconds to wait for an answer.
+ * @return LDAPResult object containing all LDAP results returned by
+ *         the query.
+ * @throws LDAPException An error occurred processing the search query.
+ */
 LDAPResult* LDAPConnection::Search(const std::string base, int scope,
 	const std::string filter, const std::vector<std::string> attrs,
 	long timeout)
@@ -117,24 +183,70 @@ LDAPResult* LDAPConnection::Search(const std::string base, int scope,
 	return new LDAPResult(this, msg);
 }
 
+/**
+ * Search for LDAP records matching a given filter. A default timeout of
+ * 30 seconds is applied.
+ *
+ * @param base    Search base to start looking from.
+ * @param scope   LDAP search scope (e.g. ONE, SUB, etc.)
+ * @param filter  Filter string (e.g. attribute=value).
+ * @param attrs   Vector of attributes to fetch in the record.
+ * @return LDAPResult object containing all LDAP results returned by
+ *         the query.
+ * @throws LDAPException An error occurred processing the search query.
+ */
 LDAPResult* LDAPConnection::Search(const std::string base, int scope,
 	const std::string filter, const std::vector<std::string> attrs)
 {
 	return Search(base, scope, filter, attrs, 30000);
 }
 
+/**
+ * Search for LDAP records matching a given filter. All attributes of any
+ * matching record are fetched.
+ *
+ * @param base    Search base to start looking from.
+ * @param scope   LDAP search scope (e.g. ONE, SUB, etc.)
+ * @param filter  Filter string (e.g. attribute=value).
+ * @param timeout Number of milliseconds to wait for a result.
+ * @return LDAPResult object containing all LDAP results returned by
+ *         the query.
+ * @throws LDAPException An error occurred processing the search query.
+ */
 LDAPResult* LDAPConnection::Search(const std::string base, int scope,
 	const std::string filter, long timeout)
 {
 	return Search(base, scope, filter, kLdapFilterAll, timeout);
 }
 
+/**
+ * Search for LDAP records matching a given filter. A default timeout of
+ * 30 seconds is applied. All attributes of any matching record are fetched.
+ *
+ * @param base    Search base to start looking from.
+ * @param scope   LDAP search scope (e.g. ONE, SUB, etc.)
+ * @param filter  Filter string (e.g. attribute=value).
+ * @return LDAPResult object containing all LDAP results returned by
+ *         the query.
+ * @throws LDAPException An error occurred processing the search query.
+ */
 LDAPResult* LDAPConnection::Search(const std::string base, int scope,
 	const std::string filter)
 {
 	return Search(base, scope, filter, kLdapFilterAll, 30000);
 }
 
+/**
+ * Search for LDAP records matching a given filter. All attributes of any
+ * matching record are fetched. A subtree search is performed.
+ *
+ * @param base    Search base to start looking from.
+ * @param filter  Filter string (e.g. attribute=value).
+ * @param timeout Number of milliseconds to wait for a result.
+ * @return LDAPResult object containing all LDAP results returned by
+ *         the query.
+ * @throws LDAPException An error occurred processing the search query.
+ */
 LDAPResult* LDAPConnection::Search(const std::string base,
 	const std::string filter, long timeout)
 {
@@ -142,6 +254,17 @@ LDAPResult* LDAPConnection::Search(const std::string base,
 		timeout);
 }
 
+/**
+ * Search for LDAP records matching a given filter. A default timeout of
+ * 30 seconds is applied. All attributes of any matching record are
+ * fetched. A subtree search is performed.
+ *
+ * @param base    Search base to start looking from.
+ * @param filter  Filter string (e.g. attribute=value).
+ * @return LDAPResult object containing all LDAP results returned by
+ *         the query.
+ * @throws LDAPException An error occurred processing the search query.
+ */
 LDAPResult* LDAPConnection::Search(const std::string base,
 	const std::string filter)
 {
